2c03afa31461fd4232cb78c3fdc343fd53e73847
swathirebbavarapu <110220876+swathirebbavarapu@users.noreply.github.com>
2022-10-14 10:44:40 +0530

    Add files via upload

---
+++ write.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const writeFile = promisify(fs.writeFile);
+(async () => {
+  try {
+    await writeFile('demofile.txt', `Hey @ ${new Date()}`);
+    console.log('File created successfully with promisify and async/await!');
+  } catch (err) {
+    console.log(err);
+  }
+})();
