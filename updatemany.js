ef9a4a0eae1cc68400afb41d50473a361f34f736
swathirebbavarapu <110220876+swathirebbavarapu@users.noreply.github.com>
2022-11-11 20:56:15 +1400

    Add files via upload

---
+++ updatemany.js
@@ -0,0 +1,26 @@
+const express =require('express');
+const app=express();
+const mongoose =require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/Database",(err)=>{
+if(err)
+console.log("DB not connected error");
+else
+console.log("DB connected");
+});
+const ns=new mongoose.Schema({
+name:String,
+age:Number,
+Id:Number,
+place:String
+});
+const old={name:'Akhila'};
+const ne={age:30};
+const nm=new mongoose.model("records",ns);
+let doc=nm.updateMany(old,ne,(err)=>{
+	if(err){
+		console.log(err);
+	}
+	else{
+		console.log("updated");
+	}
+	});
